d4d355ff17e77a481c2269013461c74d14000d20
Allison <allisoncretel@gmail.com>
2024-05-19 18:38:42 -0400

    ggs

--- init.lua
+++ init.lua
@@ -557,10 +557,6 @@ require('lazy').setup({
       --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
       local capabilities = vim.lsp.protocol.make_client_capabilities()
       capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
-      local status, jdtls = pcall(require, 'jdtls')
-
-      local root_markers = { '.git', 'mvnw', 'gradlew', 'pom.xml', 'build.gradle' }
-      local root_dir = require('jdtls.setup').find_root(root_markers)
 
       -- Enable the following language servers
       --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
@@ -584,27 +580,6 @@ require('lazy').setup({
         -- But for many setups, the LSP (`tsserver`) will work just fine
         -- tsserver = {},
         --
-        jdtls = {
-          cmd = {
-            'java',
-            '-Declipse.application=org.eclipse.jdt.ls.core.id1',
-            '-Dosgi.bundles.defaultStartLevel=4',
-            '-Declipse.product=org.eclipse.jdt.ls.core.product',
-            '-Dlog.level=ALL',
-            '-Xmx1G',
-            '-jar',
-            '/Library/Java/jdt-language-server-1.9.0-202203031534/plugins/org.eclipse.equinox.launcher_1.6.400.v20210924-0641.jar',
-            '-configuration',
-            '/Library/Java/jdt-language-server-1.9.0-202203031534/config_mac',
-            '-data',
-            vim.fn.expand '~/.cache/jdtls-workspace' .. vim.fn.fnamemodify(vim.loop.cwd(), ':p:h:t'),
-          },
-          capabilities = capabilities,
-          root_dir = root_dir,
-          settings = {
-            java = {},
-          },
-        },
 
         lua_ls = {
           -- cmd = {...},
